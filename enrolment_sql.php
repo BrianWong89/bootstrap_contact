6ab5a3015172b964befdd922795d21946dd10e58
Brian.Wong <brian.wong@alphis.net>
2017-10-09 01:28:01 +0800

    enrolment_sql
    Still needs to be refined as per Ken's requirements.

--- enrolment_sql.php
+++ enrolment_sql.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thero
- * Date: 6/10/2017
- * Time: 3:23 AM
- */
+
+require_once("vendor/autoload.php");
+
+$results = DB::query("SELECT * FROM `enrolment`");
+
+echo "<table border='1'>\r\n";
+echo "	<tr>";
+echo "		<th>Name</th>";
+echo "		<th>Enrolment</th>";
+echo "	</tr>";
+
+foreach ($results as $row) {
+    if ($row['startDate'] == "25-12-2017" && $row['endDate'] == "25-12-2017") {
+        echo "<tr>\r\n";
+        echo "	<td>" . $row['name'] . "</td>\r\n";
+        echo "	<td>" . $row['enrolmentDate'] . "</td>\r\n";
+        echo "</tr>\r\n";
+    }
+}
+echo "</table>";
+
+echo json_encode($return, JSON_PRETTY_PRINT);
