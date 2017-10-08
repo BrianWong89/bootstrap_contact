15ca863fc4fb1fe1d0c8597e050bcf23c1130b3e
Brian.Wong <brian.wong@alphis.net>
2017-10-09 01:27:07 +0800

    MeekroDB
    MeekroDB commit.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,22 @@ class ComposerStaticInit28c10774e5a4e55e74c94ec44e626030
         ),
     );
 
+    public static $classMap = array (
+        'DB' => __DIR__ . '/..' . '/sergeytsalkov/meekrodb/db.class.php',
+        'DBHelper' => __DIR__ . '/..' . '/sergeytsalkov/meekrodb/db.class.php',
+        'DBTransaction' => __DIR__ . '/..' . '/sergeytsalkov/meekrodb/db.class.php',
+        'MeekroDB' => __DIR__ . '/..' . '/sergeytsalkov/meekrodb/db.class.php',
+        'MeekroDBEval' => __DIR__ . '/..' . '/sergeytsalkov/meekrodb/db.class.php',
+        'MeekroDBException' => __DIR__ . '/..' . '/sergeytsalkov/meekrodb/db.class.php',
+        'WhereClause' => __DIR__ . '/..' . '/sergeytsalkov/meekrodb/db.class.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit28c10774e5a4e55e74c94ec44e626030::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit28c10774e5a4e55e74c94ec44e626030::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit28c10774e5a4e55e74c94ec44e626030::$classMap;
 
         }, null, ClassLoader::class);
     }
