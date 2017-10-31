b87a495f94380b90cefb42886f3de73d8de85185
BrianWong89 <brian.wong@alphis.net>
2017-10-31 15:56:02 +0800

    hw for tdy
    minor amendments needed for code to work.

--- hospital_json.php
+++ hospital_json.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 
 $data = json_decode(file_get_contents('php://input'), true);
+DB::debugMode();
 print_r($data);
 //$results = DB::query("SELECT * FROM `security`");
 //$results = DB::query("SELECT * FROM `security` WHERE age BETWEEN %i AND %i", $("#minAge").val(), $("#maxAge").val());
@@ -10,11 +11,12 @@ print_r($data);
 if ($data['race'] == "Any Race" && $data['minAge'] == "Any Age" && $data['maxAge'] == "Any Age") {
     $results = DB::query("SELECT * FROM `security`");
 }
-//if ($data['race'] == "Any Race" && $data['minAge'] == "30" && $data['maxAge'] == 90) {
-//$results = DB::query("SELECT * FROM `security` WHERE age BETWEEN %i AND %i", $data['minAge'], $data['maxAge']);
-//}
-//if ($data['race'] == "Chinese" && $data['minAge'] == "Any Age" && $data['maxAge'] == 80) {
-//$results = DB::query("SELECT * FROM `security` WHERE ethnicity=%s AND age <=%i", $data['race'], $data['maxAge']);
-//}
+//Fix it later, or tonight when you're back
+if ($data['race'] == "Any Race" && $data['minAge'] == "30" && $data['maxAge'] == 90) {
+    $results = DB::query("SELECT * FROM `security` WHERE age BETWEEN %i AND %i", $data['minAge'], $data['maxAge']);
+}
+if ($data['race'] == "Chinese" && $data['minAge'] == "Any Age" && $data['maxAge'] == 80) {
+    $results = DB::query("SELECT * FROM `security` WHERE ethnicity=%s AND age <=%i", $data['race'], $data['maxAge']);
+}
 
 echo json_encode($results, JSON_PRETTY_PRINT);
