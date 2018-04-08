28ab399b1a39f9b46ca5ff118a05213793f8097f
Brian.Wong <brian.wong@alphis.net>
2018-04-09 02:18:02 +0800

    Amended code a little but time still doesn't work.

--- insert-clinic-timings.php
+++ insert-clinic-timings.php
@@ -1,22 +1,32 @@
 <?php
 require_once("vendor/autoload.php");
 
+/* PHP homework 7 Apr  2018 */
+
+// Question b: Add in all the timings for Bedok Clinic for Monday
 $rows = array();
-for ($i = 1; $i < 9; $i++) {
+
+// For Start Time
+$start1 = new DateTime("00:00:00");
+$start2   = new DateTime("23:00:00");
+
+// For End Time
+$end1 = new DateTime("00:59:59");
+$end2   = new DateTime("23:59:59");
+
+$interval = DateInterval::createFromDateString('1 hour');
+
+$time1 = new DatePeriod($start1, $interval, $end1);
+$time2 = new DatePeriod($start2, $interval, $end2);
+
+foreach ($times as $time) {
     $rows[] = array(
         'outlet_id' => 1,
+        'day' => 1,
+        'status' => 'testing',
+        'startTime' =>  $time1->add($interval)->format('H:i:s'),
+        'endTime' => $time2->add($interval)->format('H:i:s'),
     );
-    for ($i = 1; $i < 8; $i++) {
-        $rows[] = array(
-            'day' => 1,
-        );
-    }
-    for ($i = 1; $i < 8; $i++) {
-        $rows[] = array(
-            'startTime' => 1,
-            'endTime' => 2,
-        );
-    }
 }
 
-DB::insert('outlet_timings', $rows);
+DB::insert('outlet_timing', $rows);
