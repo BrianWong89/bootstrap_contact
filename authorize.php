0d913eb60ad2345d673c4b55a986b73420421551
Brian.Wong <brian.wong@alphis.net>
2018-05-22 22:48:24 +0800

    Completed switch case hw.

--- authorize.php
+++ authorize.php
@@ -1,27 +1,38 @@
 <?php
 
-function isAuthorized ($access_type, $command) {
-    $access_type = "";
-    $command = "";
+function isAuthorized($access_type, $command)
+{
     switch ($access_type) {
         case "Operations":
             switch ($command) {
-                case "":
+                case "view":
+                case "modify":
+                    return true;
                     break;
             }
             break;
         case "Manager":
-            return $command = "add";
+            switch ($command) {
+                case "add":
+                case "view":
+                case "modify":
+                    return true;
+                    break;
+            }
             break;
         case "Admin":
-            return $command = "delete";
-            break;
-        default:
-            return "No access type found. Please try again";
+            switch ($command) {
+                case "add":
+                case "view":
+                case "modify":
+                case "delete":
+                    return true;
+                    break;
+            }
             break;
     }
+    return false;
 }
 
 $true = isAuthorized("Admin", "delete");
-
-echo $role;
+echo $true;
